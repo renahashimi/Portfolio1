5333856525b7874a0d16eae9f8c4f0370534a8de
Rena Hashimi <renhas54143@stud.noroff.no>
2024-03-03 13:17:15 +0100

    Last changes update / fix

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 /*-- MODAL --*/
-const modal = document.querySelector(".modal");
 const openModals = document.querySelectorAll(".openbtn");
 const closeModals = document.querySelectorAll(".closebtn");
 
-
+//Open modal text
 openModals.forEach(openModal => {
     openModal.addEventListener("click", () => {
         openModal.nextElementSibling.showModal();
     });
 });
 
+//Button to close Modal
 closeModals.forEach(closeModal => {
     closeModal.addEventListener("click", () => {
         closeModal.parentNode.close();
+    });
+});
+
+
+//Also coded a "window" click for easier closing.
+window.addEventListener("click", (e) => {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        if(e.target === modal) {
+            modal.close();
+        }
     });
 });
